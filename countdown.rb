8701fd6610986179e16d478a9487bea8d0512c52
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 05:23:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,11 +12,10 @@ end
 
 
 def countdown_with_sleep(num)
-  while num >
- num = "#{num}SECOND(S)!"
-  num -=1 
-  
-  sleep(1)
-  end
-  'HAPPY NEW YEAR!'
-  end 
+   while num > 0
+  puts "#{num} SECOND(S)!"
+ num -= 1
+ sleep(1)
+ end
+'HAPPY NEW YEAR!'
+end
