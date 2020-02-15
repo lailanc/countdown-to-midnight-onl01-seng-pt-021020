552a576eaee53a365e27124e40af10f63c8a3b73
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 05:04:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,14 +5,18 @@ def countdown(num)
  while num > 0
   puts num.to_s + 'SECOND(S)!'
   num = num - 1
- num-=1
+ num-= 1
  end
 'HAPPY NEW YEAR!'
-
 end
 
 
 
 def countdown_with_sleep(num)
   while num >log1
-  puts "#{num} "'SECOND(S)!'
+  puts "#{num} 'SECOND(S)!'
+  num=num - 1 
+  num -=1 
+  'HAPPY NEW YEAR!'
+  
+end 
